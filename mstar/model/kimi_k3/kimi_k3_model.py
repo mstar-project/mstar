2e4ae515749a2b1a0af47c65b2469adc5f92fc62
merceod <atj10@stanford.edu>
2026-09-16 00:29:52 -0700

    kimi_k3: model_kwargs.mixed_prefill_decode

--- mstar/model/kimi_k3/kimi_k3_model.py
+++ mstar/model/kimi_k3/kimi_k3_model.py
@@ -82,6 +82,8 @@ class KimiK3Model(Model):
         # requests per prefill step (the scheduler splits larger groups); None lifts the cap
         pre = kwargs.get("max_prefill_batch_size", 8)
         self.max_prefill_batch_size = int(pre) if pre is not None else None
+        # decode requests ride along in prefill steps instead of waiting for them
+        self.mixed_prefill_decode = bool(kwargs.get("mixed_prefill_decode", False))
         self.tokenizer = KimiK3Tokenizer(self.local_dir)
         self.config.stop_token_ids = frozenset({self.tokenizer.eos_id, self.tokenizer.eot_id})
         self._submodule_cache: dict[str, NodeSubmodule | None] = {}
@@ -279,7 +281,8 @@ class KimiK3Model(Model):
         )
         submodule = KimiK3LLMSubmodule(language_model=language_model, config=self.config, cuda_graphs=graph_safe,
                                        max_capture_batch_size=self.max_capture_batch_size,
-                                       max_prefill_batch_size=self.max_prefill_batch_size)
+                                       max_prefill_batch_size=self.max_prefill_batch_size,
+                                       mixed_prefill_decode=self.mixed_prefill_decode)
         self._submodule_cache[node_name] = submodule
         logger.info("Loaded Kimi K3 %s on %s", node_name, device)
         return submodule
