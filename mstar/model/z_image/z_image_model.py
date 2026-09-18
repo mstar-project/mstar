95d6c4713c6757ae17a22f658dd9ae59073bf05d
Merced <atj10@stanford.edu>
2026-09-18 06:12:23 -0700

    Wrap the Z-Image denoise node construction

--- mstar/model/z_image/z_image_model.py
+++ mstar/model/z_image/z_image_model.py
@@ -298,7 +298,8 @@ class ZImageModel(Model):
             return ZImageDenoiseSubmodule(
                 build_transformer(self.config, self.snapshot, device), self.config, loop_name=DENOISE_LOOP,
                 attn_resource_key=DIT_ATTN if self.attention_backend == "flashinfer" else None,
-                compile_transformer=self.compile_transformer, compile_eager_rounding=self.compile_eager_rounding, max_batch_size=self.max_batch_size,
+                compile_transformer=self.compile_transformer,
+                compile_eager_rounding=self.compile_eager_rounding, max_batch_size=self.max_batch_size,
                 capture_shapes=self.capture_shapes(), capture_batch_sizes=self.capture_batch_sizes,
             )
         logger.warning("Z-Image has no submodule for node %r; running it dummy", node_name)
