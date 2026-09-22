baa06ac5109f93b3eaad14f1aadb3998a6fa113a
merceod <atj10@stanford.edu>
2026-09-22 02:42:51 -0700

    lint: imports in order in __init__.py

--- mstar/engine/resources/speculative/__init__.py
+++ mstar/engine/resources/speculative/__init__.py
@@ -1,4 +1,4 @@
-from mstar.engine.resources.speculative.config import SPEC_ACCEPTANCE, SpecAccepted, SpecAcceptanceSpec, SpecStep
+from mstar.engine.resources.speculative.config import SPEC_ACCEPTANCE, SpecAcceptanceSpec, SpecAccepted, SpecStep
 from mstar.engine.resources.speculative.resource import SpecAcceptance
 
 __all__ = ["SPEC_ACCEPTANCE", "SpecAccepted", "SpecAcceptance", "SpecAcceptanceSpec", "SpecStep"]
